769ac35c7d156725d84862237fc9eaeef5409168
Yene_me <myeneneh@hotmail.com>
2013-06-18 23:30:02 +0100

    find connection

--- gen/com/yene/example/bustiming/R.java
+++ gen/com/yene/example/bustiming/R.java
@@ -16,8 +16,9 @@ public final class R {
     public static final class id {
         public static final int busN=0x7f070000;
         public static final int button1=0x7f070001;
-        public static final int label=0x7f070002;
-        public static final int menu_settings=0x7f070003;
+        public static final int button2=0x7f070002;
+        public static final int label=0x7f070003;
+        public static final int menu_settings=0x7f070004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -29,6 +30,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int menu_settings=0x7f040001;
+        public static final int near_by_stop=0x7f040008;
         public static final int search_button_text=0x7f040003;
         public static final int search_hint=0x7f040004;
         public static final int search_title=0x7f040002;
--- src/com/yene/bustiming/MainActivity.java
+++ src/com/yene/bustiming/MainActivity.java
@@ -2,10 +2,14 @@ package com.yene.bustiming;
 
 import com.yene.example.bustiming.R;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.NetworkInfo.State;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
@@ -13,11 +17,21 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	 public final static String BUS_NO_MESSAGE = "com.yene.BUSNUMBER";
+	 private boolean isConnected = false;
+	 Context context;
+	 State wifi;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		context = getApplicationContext();
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ConnectivityManager cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		
+		wifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState();
+		if (wifi == NetworkInfo.State.CONNECTED || wifi == NetworkInfo.State.CONNECTING) {
+			Log.d("isConnection",""+isConnected);
+		}    isConnected = true;
 	}
 
 	@Override
@@ -32,9 +46,32 @@ public class MainActivity extends Activity {
 	    EditText editText = (EditText) findViewById(R.id.busN);
 	    String search_term = editText.getText().toString();
 	    
+	    if(search_term.length()<1 || search_term.length()>5 && isConnected ){
+		    
+			CharSequence text = "Please Enter Bus Number";
+			int duration = Toast.LENGTH_SHORT;
+			Toast toast = Toast.makeText(context, text, duration);
+			toast.show();
+	    }else if(!isConnected){
+	    	CharSequence text = "Need Internet Connection";
+			int duration = Toast.LENGTH_SHORT;
+			Toast toast = Toast.makeText(context, text, duration);
+			toast.show();
+	    	
+	    }else if(isConnected){
+	    	 intent.putExtra(BUS_NO_MESSAGE, search_term);
+	 	    startActivity(intent);
+	    }
+	}
+	
+	public void findNearByStop (View view){
+		Intent intent = new Intent(this, Direction.class);
+	    EditText editText = (EditText) findViewById(R.id.busN);
+	    String search_term = editText.getText().toString();
+	    
 	    if(search_term.length()<1 || search_term.length()>5){
 		    Context context = getApplicationContext();
-			CharSequence text = "Please Enter Bus Number";
+			CharSequence text = "Searching for you Location";
 			int duration = Toast.LENGTH_SHORT;
 			Toast toast = Toast.makeText(context, text, duration);
 			toast.show();
@@ -42,11 +79,6 @@ public class MainActivity extends Activity {
 	    	 intent.putExtra(BUS_NO_MESSAGE, search_term);
 	 	    startActivity(intent);
 	    }
-		
-		
-	   
-
-		
 	}
 
 }
